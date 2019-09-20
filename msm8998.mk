4c9fc1ec999842b1e8c552c10b0440feaba64434
Pranav Vashi <neobuddy89@gmail.com>
2019-09-21 00:13:08 +0530

    msm8998-common: Setup device soong namespace
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ibea45e9ab4a55faf3655ed095041ada167560071

--- msm8998.mk
+++ msm8998.mk
@@ -384,6 +384,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    device/nubia/msm8998-common
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
